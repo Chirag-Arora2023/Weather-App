cfe4ca0c88ae84d24389ca98087e954085b13024
Chirag Arora <cse.chirag.arora@gmail.com>
2023-11-05 12:26:30 +0530

    JS-first-step

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
-const API_key="0ca512df00a9ae3d090fa3b20e6ccd42";
+const apiCoordUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+const API_KEY="0ca512df00a9ae3d090fa3b20e6ccd42";
+//https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png
+//http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png
 
-function renderWeatherInfo(data){
-    let newPara = document.createElement('h2');
-    newPara.textContent=`${data?.main?.temp.toFixed(2)}°C`;
-    document.body.appendChild(newPara);
-}
 
-async function fetchWeatherInfo(){
-    let lat = 29.685629;
-    let lon = 76.990547;
-    let city = "karnal";
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
-    const data = await response.json();
-
-    console.log(city +"'s Weather: ", data);
-
-    renderWeatherInfo(data);
-
-}
